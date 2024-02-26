ba6826e82b003ef6533bfbe86e89778afb57a958
tiimsvk <52893640+tiimsvk@users.noreply.github.com>
2024-02-26 08:36:23 +0100

    Add files via upload

---
+++ automation.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+#include "sps30.h"
+
+namespace esphome {
+namespace sps30 {
+
+template<typename... Ts> class StartFanAction : public Action<Ts...> {
+ public:
+  explicit StartFanAction(SPS30Component *sps30) : sps30_(sps30) {}
+
+  void play(Ts... x) override { this->sps30_->start_fan_cleaning(); }
+
+ protected:
+  SPS30Component *sps30_;
+};
+
+//template<typename... Ts> class StopMeasureAction : public Action<Ts...> {
+// public:
+//  explicit StopMeasureAction(SPS30Component *sps30) : sps30_(sps30) {}
+
+//  void play(Ts... x) override { this->sps30_->stop_measure(); }
+
+// protected:
+//  SPS30Component *sps30_;
+//};
+
+}  // namespace sps30
+}  // namespace esphome
